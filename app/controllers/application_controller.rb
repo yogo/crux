87c56d4db7e6fbf54bbc9994f2c3084f294ba0ea
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-30 16:03:57 -0600

    Adding a basic auth filter for CRUX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   use Rails::DataMapper::Middleware::IdentityMap
   protect_from_forgery
   
+  before_filter :basic_auth
+  
+  def basic_auth
+    # If it's local, let it go. For development
+    return true if ["127.0.0.1", "0:0:0:0:0:0:0:1%0"].include?(request.env["REMOTE_ADDR"])
+    authenticate_or_request_with_http_basic do |id, password|
+      id == 'crux' && password == 'preston'
+    end
+  end
+  
 end
