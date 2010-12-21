4178c84eaf5fca13e29279430247c2000181a884
Pol Llovet <pol.llovet@gmail.com>
2010-12-21 14:06:58 -0700

    Use rspec as the testing framework

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,10 @@ module Yogo
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
+    
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
 
