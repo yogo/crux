be4e005abe0cae40bda7abee9909c9dfe491f933
Pol Llovet <pol.llovet@gmail.com>
2011-01-11 10:03:17 -0700

    remove the cache expiration for project creation

--- app/controllers/yogo/projects_controller.rb
+++ app/controllers/yogo/projects_controller.rb
@@ -48,9 +48,10 @@ class Yogo::ProjectsController < Yogo::BaseController
   #     format.html
   #   end
   # end
+  # 
   
   def create
-    expire_action :action => :show
+    # expire_action :action => :show
     create! do |success, failure|
       success.html { redirect_to kefed_library_yogo_project_url(@project.id.to_s) }
     end
